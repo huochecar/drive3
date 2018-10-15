700a02ebbab2bb795148f5b7ec10d770977fe6b2
needones <needones@aliyun.com>
2018-10-15 20:08:59 +0800

    get animals

--- App/views.py
+++ App/views.py
@@ -1,10 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-
+from App.models import DriverCar
 
 blue = Blueprint('blue',__name__)
 
 @blue.route('/')
 def hello():
     return '123'
+@blue.route('/get/')
+def get():
+    animals = DriverCar.query.all()
+    return render_template('get.html',animals=animals)
 
